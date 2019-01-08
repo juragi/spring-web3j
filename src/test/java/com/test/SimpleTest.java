31932cdef5a83a37b226ea511faceed6416289fa
juragi <juragi21@hotmail.com>
2019-01-08 18:24:53 +0900

    Ethereum wallet generate test

--- src/test/java/com/test/SimpleTest.java
+++ src/test/java/com/test/SimpleTest.java
@@ -1,5 +1,10 @@
 package com.test;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.HashMap;
 
 import org.junit.Test;
@@ -7,6 +12,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
 
 import com.web.service.UserService;
 
@@ -17,10 +24,14 @@ public class SimpleTest {
 	UserService userService;
 	
 	@Test
-	public void test() {
-		System.out.println("this is test");
-		HashMap<String, Object> params = new HashMap<String, Object>();
-		params.put("email", "test@test.test");
-		System.out.println(userService.getUserInfo(params));
+	public void test() throws Exception {
+		System.out.println("wallet generate test");
+		File location = new File("D:/upload");
+		if(!location.exists()) location.mkdir();
+		
+		String result = WalletUtils.generateNewWalletFile("testtest1", location);
+		System.out.println(result);
+		Credentials c = WalletUtils.loadCredentials("testtest1", new File(location.getPath()+ "/" + result));
+		System.out.println(c.getAddress());
 	}
 }
